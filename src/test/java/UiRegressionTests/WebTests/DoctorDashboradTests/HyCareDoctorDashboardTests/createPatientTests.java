fed8b059d71221cc42c338894d85a543ba11239a
testdummytest <163297472+testdummytest@users.noreply.github.com>
2024-03-26 16:04:14 +0530

    Update and rename createPatient.java to createPatientTests.java

--- src/test/java/UiRegressionTests/WebTests/DoctorDashboradTests/HyCareDoctorDashboardTests/createPatientTests.java
+++ src/test/java/UiRegressionTests/WebTests/DoctorDashboradTests/HyCareDoctorDashboardTests/createPatientTests.java
@@ -12,7 +12,7 @@ import PageObjects.PatientHomePage;
 import PageObjects.TestingUtilPage;
 import UiRegressionTests.ChLoginBaseTest;
 
-public class createPatient extends ChLoginBaseTest {
+public class createPatientTests extends ChLoginBaseTest {
 
     @Test(dataProvider = "create-hycare-patient-by-doctor-data", dataProviderClass = DataProviderClass.class)
     public void shouldVerifyThatTheDoctorCanCreatePatientSuccessfully(Patient patient, Admin admin, Doctor doctor) {
